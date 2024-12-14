d6ecac29fb167c11a6ef487195be985e4f07dfb1
Jefferlol <jeffersonzambrano777@gmail.com>
2024-12-13 23:37:01 -0500

    agregando carrito

--- RestobarWeb/fronted/basedatos.js
+++ RestobarWeb/fronted/basedatos.js
@@ -1,3 +1,5 @@
+let carrito = []; // Declarar carrito como un arreglo vacío
+
 // Función para cargar productos según la categoría seleccionada
 function loadProducts(categoria) {
     const url = `http://localhost:3000/categoria/${categoria}`;
@@ -29,16 +31,18 @@ function displayProducts(products) {
     }
 
     products.forEach(product => {
+        const imagenURL = `http://localhost:3000/imagenes/${product.idproducto}`    
         const productElement = document.createElement('div');
         productElement.classList.add('product');
 
         productElement.innerHTML = `
             <div class="product-image">
-                <img src="http://localhost:3000/imagenes/${product.idproducto}" alt="${product.nombreproducto}" />
+                <img src="${imagenURL}" alt="${product.nombreproducto}" />
             </div>
             <div class="product-info">
                 <h3>${product.nombreproducto}</h3>
                 <p>Precio: S/${product.precio}</p>
+                <button onclick="addToCart(${product.idproducto}, '${product.nombreproducto}', ${product.precio}, '${imagenURL}')">Agregar al carrito</button>
             </div>
         `;
 
@@ -46,3 +50,21 @@ function displayProducts(products) {
     });
 }
 
+function addToCart(id, nombre, precio, imagen) {
+    // Verificar si el producto ya está en el carrito
+    const productoExistente = carrito.find(producto => producto.id === id);
+    if (productoExistente) {
+        productoExistente.cantidad += 1;
+    } else {
+        carrito.push({ id, nombre, precio, imagen, cantidad: 1 });
+    }
+
+    // Actualizar el contador del carrito
+    const carritoCountElement = document.getElementById('carrito-count');
+    if (carritoCountElement) {
+        carritoCountElement.textContent = carrito.length;
+    }
+
+    alert(`${nombre} ha sido agregado al carrito`);
+    console.log("soy un let" + carrito)
+}
