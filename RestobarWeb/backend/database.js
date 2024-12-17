c40a1bc8cff0460d6c8ea34629fd94d5fdafcff2
wildev <wildeveloper21@gmail.com>
2024-12-17 00:10:55 -0500

    Login - Pedidos

--- RestobarWeb/backend/database.js
+++ RestobarWeb/backend/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password',
+    password: '123456',
     database: 'db_saborlatino'
 });
 
